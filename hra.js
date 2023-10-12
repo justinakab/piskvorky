dcb487f1434c324f82451d8e13b972f3f5b4ede1
Justina Kabelková <justina.kabel@gmail.com>
2023-10-12 13:47:51 +0200

    cast 3 + animace

--- hra.js
+++ hra.js
@@ -1,3 +1,5 @@
+import { findWinner } from 'https://unpkg.com/piskvorky@0.1.4';
+
 let currentPlayer = 'circle';
 const imagePlayer = document.querySelector('.image-player');
 const repeatButton = document.querySelector('#restart-button');
@@ -6,57 +8,18 @@ const odehrani = (event) => {
   if (currentPlayer === 'circle') {
     event.target.classList.toggle('board__field--circle');
     currentPlayer = 'cross';
-    imagePlayer.src = 'images/circle.svg';
+    imagePlayer.src = 'images/cross.svg';
     event.target.disabled = true;
   } else {
     event.target.classList.toggle('board__field--cross');
     currentPlayer = 'circle';
-    imagePlayer.src = 'images/cross.svg';
+    imagePlayer.src = 'images/circle.svg';
     event.target.disabled = true;
   }
+  evaluation();
 };
 
-const pole1 = document
-  .querySelector('.playground__button:nth-child(1)')
-  .addEventListener('click', odehrani);
-
-const pole2 = document
-  .querySelector('.playground__button:nth-child(2)')
-  .addEventListener('click', odehrani);
-
-const pole3 = document
-  .querySelector('.playground__button:nth-child(3)')
-  .addEventListener('click', odehrani);
-
-const pole4 = document
-  .querySelector('.playground__button:nth-child(4)')
-  .addEventListener('click', odehrani);
-
-const pole5 = document
-  .querySelector('.playground__button:nth-child(5)')
-  .addEventListener('click', odehrani);
-
-const pole6 = document
-  .querySelector('.playground__button:nth-child(6)')
-  .addEventListener('click', odehrani);
-
-const pole7 = document
-  .querySelector('.playground__button:nth-child(7)')
-  .addEventListener('click', odehrani);
-
-const pole8 = document
-  .querySelector('.playground__button:nth-child(8)')
-  .addEventListener('click', odehrani);
-
-const pole9 = document
-  .querySelector('.playground__button:nth-child(9)')
-  .addEventListener('click', odehrani);
-
-const pole10 = document
-  .querySelector('.playground__button:nth-child(10)')
-  .addEventListener('click', odehrani);
-
-// bonus 1
+// alerts na reload
 const varovani = (event) => {
   const confirmation = window.confirm('Opravdu chceš začít znovu?');
   if (confirmation) {
@@ -67,3 +30,49 @@ const varovani = (event) => {
 };
 
 repeatButton.addEventListener('click', varovani);
+
+// část 3
+
+const buttons = document.querySelectorAll('.playground__button');
+
+buttons.forEach((button) => {
+  button.addEventListener('click', odehrani);
+});
+
+// evaluace herního postupu
+const evaluation = () => {
+  let field = [];
+  buttons.forEach((button) => {
+    if (button.classList.contains('board__field--circle')) {
+      field.push('o');
+    } else if (button.classList.contains('board__field--cross')) {
+      field.push('x');
+    } else {
+      field.push('_');
+    }
+  });
+
+  // spuštění funkce
+  const winner = findWinner(field);
+
+  // časovače odpovědí
+  if (winner === 'o' || winner === 'x') {
+    // alert
+    setTimeout(() => {
+      alert(`Vyhrál hráč se symbolem ${winner}.`);
+    }, 500);
+    // reload hry
+    setTimeout(() => {
+      location.reload();
+    }, 1000);
+  }
+
+  if (winner === 'tie') {
+    setTimeout(() => {
+      alert(`Hra skončila remízou.`);
+    }, 500);
+    setTimeout(() => {
+      location.reload();
+    }, 1000);
+  }
+};
